0a24beb498e53f8e3353d40c4610546456a358fe
Stente3 <stentem0103@gmail.com>
2024-10-13 18:41:45 -0500

    Added delete and edit functionality in the catalog store and admin panel
    Description:
    - Added new functions in the Zustand store to delete a catalog item and modify product attributes.
    - Implemented necessary changes to ensure that the delete and edit buttons work properly in the admin panel.

--- frontend/src/stores/productStore.js
+++ frontend/src/stores/productStore.js
@@ -5,6 +5,16 @@ const useProductsStore = create(set => ({
 	hasFetched: false, // Inicialmente es false, ya que no se ha hecho el fetch
 	updateAllProducts: newProducts => set({ products: newProducts }),
 	setHasFetched: status => set(() => ({ hasFetched: status })), // Función para actualizar `hasFetched`
+
+	// función para eliminar producto por id
+	removeProductById: (id) => set((state) => ({ products: state.products.filter(product => product.id !== id)})),
+
+	// Función para actualizar producto por id
+	updateProductById: (id, updatedInfo) => set((state) => ({
+		products: state.products.map(product => 
+			product.id === id ? { ...product, ...updatedInfo } : product
+		)
+	}))
 }));
 
 export default useProductsStore;
--- frontend/src/views/ProductAdminPanel.jsx
+++ frontend/src/views/ProductAdminPanel.jsx
@@ -1,14 +1,65 @@
+import { useState } from 'react';
 import useProductsStore from '../stores/productStore';
 import { useDataFetch } from '../hooks/useDataFetch';
 import { Nav } from '../components/Nav';
 
 const ProductAdminPanel = () => {
+	const [editingProductId, setEditingProductId] = useState(null);
+	const [updatedProduct, setUpdatedProduct] = useState({});
+
 	const url = import.meta.env.VITE_API_URL;
 	// Utiliza el hook para obtener datos
 	const { loading, error } = useDataFetch(url);
 
 	// Obtenemos los productos del estado global de Zustand
-	const { products } = useProductsStore();
+	const { products, removeProductById, updateProductById } = useProductsStore();
+
+	const handleRemove = id => {
+		removeProductById(id);
+	};
+
+	const handleEdit = product => {
+		setEditingProductId(product.id);
+		setUpdatedProduct(product); // Copia los valores del producto actual para editarlos
+	};
+
+	const handleInputChange = e => {
+		const { name, value } = e.target;
+		// Verificamos si el name es 'talla'
+		if (name === 'talla') {
+			// Convertimos el value en un arreglo si contiene tallas
+			const sizesArray = value.split(',').map(size => size.trim());
+			setUpdatedProduct(prevState => ({
+				...prevState,
+				[name]: sizesArray, // Asignamos el array de tallas
+			}));
+		} else {
+			setUpdatedProduct(prevState => ({
+				...prevState,
+				[name]: value,
+			}));
+		}
+	};
+
+	const handleImageChange = e => {
+		const file = e.target.files[0];
+		if (file) {
+			const reader = new FileReader();
+			reader.onloadend = () => {
+				setUpdatedProduct(prevState => ({
+					...prevState,
+					imagen: reader.result,
+				}));
+			};
+			reader.readAsDataURL(file);
+		}
+	};
+
+	const handleSave = (id, product) => {
+		// Aquí actualizarías el producto en la store y lo guardas.
+		updateProductById(id, product);
+		setEditingProductId(null); // Salir del modo de edición
+	};
 
 	return (
 		<>
@@ -21,95 +72,142 @@ const ProductAdminPanel = () => {
 			<table className='border-collapse w-full'>
 				<thead>
 					<tr>
-						<th className='p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell'>
-							Nombre
-						</th>
-						<th className='p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell'>
-							Descripción
-						</th>
-						<th className='p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell'>
-							Precio
-						</th>
-						<th className='p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell'>
-							Color
-						</th>
-						<th className='p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell'>
-							Talla
-						</th>
-						<th className='p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell'>
-							Categoría
-						</th>
-						<th className='p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell'>
-							Imagen
-						</th>
-						<th className='p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell'>
-							Acciones
-						</th>
+						{[
+							'Nombre',
+							'Descripción',
+							'Precio',
+							'Color',
+							'Talla',
+							'Categoría',
+							'Imagen',
+							'Acciones',
+						].map(header => (
+							<th
+								key={header}
+								className='p-3 font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell'
+							>
+								{header}
+							</th>
+						))}
 					</tr>
 				</thead>
 				<tbody>
-					{products.map(product => (
+					{products.map((product, index) => (
 						<tr
-							className='bg-white lg:hover:bg-gray-100 flex lg:table-row flex-row lg:flex-row flex-wrap lg:flex-no-wrap mb-10 lg:mb-0'
 							key={product.id}
+							className='bg-white lg:hover:bg-gray-100 flex lg:table-row flex-row lg:flex-row flex-wrap lg:flex-no-wrap mb-10 lg:mb-0'
 						>
 							<td className='w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static'>
-								<span className='lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase'>
-									Nombre
-								</span>
-								{product.nombre}
+								{editingProductId === product.id ? (
+									<input
+										type='text'
+										name='nombre'
+										value={updatedProduct.nombre}
+										onChange={handleInputChange}
+										className='border p-1 w-full'
+									/>
+								) : (
+									product.nombre
+								)}
 							</td>
-							<td className='w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static'>
-								<span className='lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase'>
-									Descripción
-								</span>
-								{product.descripcion}
+							<td className='w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static'>
+								{editingProductId === product.id ? (
+									<textarea
+										name='descripcion'
+										value={updatedProduct.descripcion}
+										onChange={handleInputChange}
+										className='border p-1 w-full'
+									/>
+								) : (
+									product.descripcion
+								)}
 							</td>
-							<td className='w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static'>
-								<span className='lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase'>
-									Precio
-								</span>
-								${product.precio}
+							<td className='w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static'>
+								{editingProductId === product.id ? (
+									<input
+										type='number'
+										name='precio'
+										value={updatedProduct.precio}
+										onChange={handleInputChange}
+										className='border p-1 w-full'
+									/>
+								) : (
+									`$${product.precio}`
+								)}
 							</td>
-							<td className='w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static'>
-								<span className='lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase'>
-									Color
-								</span>
-								{product.color}
+							<td className='w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static'>
+								{editingProductId === product.id ? (
+									<input
+										type='text'
+										name='color'
+										value={updatedProduct.color}
+										onChange={handleInputChange}
+										className='border p-1 w-full'
+									/>
+								) : (
+									product.color
+								)}
 							</td>
-							<td className='w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static'>
-								<span className='lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase'>
-									Talla
-								</span>
-								{product.talla.join(', ')}
+							<td className='w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static'>
+								{editingProductId === product.id ? (
+									<input
+										type='text'
+										name='talla'
+										value={updatedProduct.talla}
+										onChange={handleInputChange}
+										className='border p-1 w-full'
+									/>
+								) : (
+									product.talla.join(', ')
+								)}
 							</td>
-							<td className='w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static'>
-								<span className='lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase'>
-									Categoría
-								</span>
-								{product.categoria}
+							<td className='w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static'>
+								{editingProductId === product.id ? (
+									<input
+										type='text'
+										name='categoria'
+										value={updatedProduct.categoria}
+										onChange={handleInputChange}
+										className='border p-1 w-full'
+									/>
+								) : (
+									product.categoria
+								)}
 							</td>
-							<td className='w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static flex justify-center'>
-								<span className='lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase'>
-									Imagen
-								</span>
-								<img
-									src={product.imagen}
-									alt={product.descripcion}
-									className='h-16 w-16'
-								/>
+							<td className='w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static flex justify-center'>
+								{editingProductId === product.id ? (
+									<input
+										type='file'
+										onChange={handleImageChange}
+										className='border p-1 w-full'
+									/>
+								) : (
+									<img
+										src={product.imagen}
+										alt={product.descripcion}
+										className='h-16 w-16'
+									/>
+								)}
 							</td>
-							<td className='w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static'>
-								<span className='lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase'>
-									Acciones
-								</span>
+							<td className='w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static'>
+								{editingProductId === product.id ? (
+									<button
+										className='text-green-400 hover:text-green-600'
+										onClick={() => handleSave(product.id, updatedProduct)}
+									>
+										Guardar
+									</button>
+								) : (
+									<button
+										className='text-blue-400 hover:text-blue-600'
+										onClick={() => handleEdit(product)}
+									>
+										Editar
+									</button>
+								)}
 								<button
-									className='text-blue-400 hover:text-blue-600'
-								>
-									Editar
-								</button>
-								<button
-									className='text-blue-400 hover:text-blue-600 pl-6'
+									className='text-red-400 hover:text-red-600 pl-6'
+									onClick={() => handleRemove(product.id)}
 								>
 									Eliminar
 								</button>
