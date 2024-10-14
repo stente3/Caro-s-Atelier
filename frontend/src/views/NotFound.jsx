7f8788739b67adbdd816a0b7ae4984e7de884e5f
Stente3 <stentem0103@gmail.com>
2024-10-14 09:54:07 -0500

    Improved 404 "Not Found" view with image and navigation
    Description:
    - Added an image to the 404 "Not Found" view.
    - Enhanced the 404 view to be more user-friendly, allowing users to return to the homepage with a single click using React Router.

--- frontend/src/views/NotFound.jsx
+++ frontend/src/views/NotFound.jsx
@@ -1,16 +1,20 @@
-import { useRouteError } from 'react-router-dom';
+import { useRouteError, Link } from 'react-router-dom';
+import NotFoundImage from '../assets/404.png';
 
 export function NotFound() {
 	const error = useRouteError();
 	console.error(error);
 
 	return (
-		<div id='error-page' className='text-center'>
-			<h1 className='font-bold'>Oops!</h1>
-			<p>Sorry, an unexpected error has occurred.</p>
-			<p>
-				<i>{error.statusText || error.message}</i>
-			</p>
+		<div
+			id='error-page'
+			className='bg-[#D9D9D9] flex w-screen justify-center items-center h-screen flex-col pb-20'
+		>
+			<img src={NotFoundImage} alt='Not Found Image' />
+			<p>Lo sentimos, la página que buscas no se encuentra disponible.</p>
+			<Link to={'/'}>
+				<button className='text-white bg-black px-4 py-2 border rounded-md mt-4'>Volver al inicio</button>
+			</Link>
 		</div>
 	);
 }
